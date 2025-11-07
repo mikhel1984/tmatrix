f8739e9d71377259794be170dd2f7283f62130eb
Stanislav <mikhel.sk@gmail.com>
2025-11-07 17:03:01 +0300

    Minor improvements

--- src/lib/tmatrix_transform.c
+++ src/lib/tmatrix_transform.c
@@ -7,9 +7,8 @@
 
 int tf_chol(tMat* dst, tMat* m, int* err)
 {
-  int e = 0, pos_def = 1;
-  tmSize i, j, k, n;
-  tmVal s = 0;
+  int e = 0, pos_def = 1, i, j, k, n;
+  tmVal s = 0, *row_i, *row_j;
 
   TM_ASSERT_ARGS(m && dst && m != dst, e, end_chol);
 
@@ -17,25 +16,22 @@ int tf_chol(tMat* dst, tMat* m, int* err)
     n = m->rows;
     if (tm_relevant(dst,n,n,&e)) {
       /* clear */
-      for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) 
-          *tm_at(dst,i,j) = 0;
-      }
+      tm_zeros(dst);
       /* find lower left part */
       for (i = 0; i < n; i++) {
+        row_i = dst->data + i*n;
         for (j = 0; j <= i; j++) {
+          row_j = dst->data + j*n;
           s = *tm_at(m, i, j);
-          for (k = 0; k < j; k++) {
-            s -= (*tm_at(dst,j,k)) * (*tm_at(dst,i,k));
-          }
+          for (k = 0; k < j; k++) 
+            s -= row_i[k] * row_j[k];
           if (j < i) {
-            *tm_at(dst,i,j) = s / (*tm_at(dst,j,j));
+            row_i[j] = s / row_j[j];
           } else {
             if (s < 0) {
-              pos_def = 0;
-              goto end_chol;
+              pos_def = 0; goto end_chol;
             }
-            *tm_at(dst,i,j) = sqrt(s);
+            row_i[j] = sqrt(s);
           }
         }
       }
